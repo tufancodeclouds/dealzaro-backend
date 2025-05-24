598582eca8450d9d32b6604206efdd056551c094
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-05-24 11:46:20 +0530

    first commit

--- server.js
+++ server.js
@@ -14,9 +14,26 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
+// allowed origins
+const allowedOrigins = [
+    'https://dealzaro-admin.vercel.app',
+    'https://dealzaro.vercel.app'
+];
+
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+}));
 
 // api endpoints
 app.use('/api/user',userRouter)
