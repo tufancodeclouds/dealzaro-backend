f5be5d33903b3de0cca6efacb1ad612423671cf6
Tufan Ghosh <tufan.ghosh@codeclouds.co.in>
2025-05-31 17:00:41 +0530

    cart functionality changes

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ connectCloudinary()
 
 // allowed origins
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174'
+  'https://dealzaro.vercel.app',
+  'https://dealzaro-admin.vercel.app'
 ];
 
 // middlewares
